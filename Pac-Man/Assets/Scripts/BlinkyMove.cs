96c97ca45ff9607f0f5e865225a5b9b22b291177
HummingB <nicosadeghi@yahoo.fr>
2017-04-25 10:48:20 +0100

    Project under Unity
    Because it's easier.

---
+++ Pac-Man/Assets/Scripts/BlinkyMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkyMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public float speed = 0.3f;
+		
+	void FixedUpdate () {
+
+/*		// Animation
+		Vector2 dir = waypoints[cur].position - transform.position;
+		GetComponent<Animator>().SetFloat("DirX", dir.x);
+		GetComponent<Animator>().SetFloat("DirY", dir.y);
+		*/
+	}
+
+	void OnTriggerEnter2D(Collider2D co) {
+		if (co.name == "pacman")
+			Destroy(co.gameObject);
+	}
+}
