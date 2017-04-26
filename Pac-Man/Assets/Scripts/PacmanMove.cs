1b2b71310955c117eb214671b8e30c0e37f37b75
HummingB <nicosadeghi@yahoo.fr>
2017-04-26 20:30:15 +0100

    Maze Resize + Anims

--- Pac-Man/Assets/Scripts/PacmanMove.cs
+++ Pac-Man/Assets/Scripts/PacmanMove.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PacmanMove : MonoBehaviour {
 	public float speed = 0.4f;
 	Vector2 dest = Vector2.zero;
-    Vector2 radius = new Vector2(1f, 1f);
-    float tileSize = 2.5f;
+    Vector2 radius = new Vector2(0.5f, 0.5f);
+    float tileSize = 1f;
     public Vector2 dir;
 
     // Use this for initialization
@@ -27,13 +27,13 @@ public class PacmanMove : MonoBehaviour {
 
         // Check for Input if not moving
         if ((Vector2)transform.position == dest) {
-			if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up + radius))
+			if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
 				dest = (Vector2)transform.position + Vector2.up*tileSize;
-			if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right + radius))
+			if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
 				dest = (Vector2)transform.position + Vector2.right*tileSize;
-			if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up - radius))
+			if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
 				dest = (Vector2)transform.position - Vector2.up*tileSize;
-			if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right - radius))
+			if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
 				dest = (Vector2)transform.position - Vector2.right*tileSize;
 		}
 
@@ -47,7 +47,7 @@ public class PacmanMove : MonoBehaviour {
 		// Cast Line from 'next to Pac-Man' to 'Pac-Man'
 		Vector2 pos = transform.position;
 		RaycastHit2D hit = Physics2D.Linecast(pos + dir , pos);
-        //Debug.Log(hit.collider);
+        Debug.Log(hit.collider);
 		return (hit.collider == GetComponent<Collider2D>());
 	}
     public Vector2 getDir()
